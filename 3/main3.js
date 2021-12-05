168743715d38005af76f6d8c89504a7cc78b9e3a
emblafant <embla98wallen@gmail.com>
2021-12-05 15:18:19 +0100

    third solution

--- 3/main3.js
+++ 3/main3.js
@@ -1,5 +1,20 @@
 const fs = require("fs");
 const text = fs.readFileSync("./input-aoc3.txt").toString("utf-8");
-const textByLine = text.split("\n").map(str=>String(str));
+const textByLine = text.split("\n").map(str=>String(str)).map(i=>i.split(" "));
 
-console.log(textByLine);
+
+let depth = 0;
+let position = 0;
+
+for (i = 0; i < textByLine.length; i++) {
+  if (textByLine[i][0] == "forward") {
+    position += Number(textByLine[i][1]);
+  } else if (textByLine[i][0] == "down") {
+    depth += Number(textByLine[i][1])
+  } else if (textByLine[i][0] == "up") {
+    depth -= Number(textByLine[i][1])
+  }
+}
+
+const product = depth * position;
+console.log(product)
